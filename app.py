00796081756e32e28da4ccfba1f50668c158a77e
Kevin Cho <takearest118@gmail.com>
2019-04-05 23:16:28 +0900

    set requirements

---
+++ app.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+from sanic import Sanic
+from sanic.response import json
+
+
+app = Sanic()
+
+
+@app.route('/')
+async def test(request):
+	return json({'hello': 'Lamborghini'})
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8000)
+
